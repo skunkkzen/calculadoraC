44b591ad51ecdbb4029617cdf928e2ab897b6643
Victor <victordestefani@hotmail.com>
2019-10-28 21:32:20 +0000

    SOMA

--- calculadora.c
+++ calculadora.c
@@ -3,10 +3,20 @@
 
 void soma()
 { 
-    //todo
+    int A;
+    int B;
+    int soma;
+
+    printf( "Escreva o primeiro numero: \n");
+    scanf("%d",&A);
+    printf (" Escreva o segundo numero: \n");
+    scanf("%d", &B);
+    soma=A+B;
+    printf( "O valor total: %d\n", soma);
+
     system ("pause"); 
     system("cls"); 
-    menu(); 
+    menu();  
 }
 
 void subtrai()
